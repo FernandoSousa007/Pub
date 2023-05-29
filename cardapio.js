d37259c30426e32586256ea2b4d1c38a42b5c1e7
Meliodasf143 <80070477+Meliodasf143@users.noreply.github.com>
2023-05-28 21:49:19 -0300

    Add files via upload

---
+++ cardapio.js
@@ -0,0 +1,165 @@
+p1=0
+p2=0
+p3=0
+p4=0
+p5=0
+p6=0
+p7=0
+p8=0
+p9=0
+p10=0
+p11=0
+t=0
+function adicionar(){
+    a=a+1
+    quantidade.innerHTML = a
+
+}
+function diminuir(){
+        quantidade.innerHTML = a
+
+   
+    }
+    function adicionar2(){
+        p2=p2+1
+        quantidade2.innerHTML = p2
+    
+    }
+    function diminuir2(){
+            p2=p2-1
+            if(p2<0){
+                alert("Adicione no minimo 1 produto")
+                p2=0
+            }
+            quantidade2.innerHTML = p2
+        }
+        function adicionar3(){
+            p3=p3+1
+            quantidade3.innerHTML = p3
+        
+        }
+        function diminuir3(){
+                p3=p3-1
+                if(p3<0){
+                    alert("Adicione no minimo 1 produto")
+                    p3=0
+                }
+                quantidade3.innerHTML = p3
+            }
+            function adicionar4(){
+                p4=p4+1
+                quantidade4.innerHTML = p4
+            
+            }
+            function diminuir4(){
+                    p4=p4-1
+                    if(p4<0){
+                        alert("Adicione no minimo 1 produto")
+                        p4=0
+                    }
+                    quantidade4.innerHTML = p4
+                }
+                function adicionar5(){
+                    p5=p5+1
+                    quantidade5.innerHTML = p5
+            
+                }
+                function diminuir5(){
+                        p5=p5-1
+                        if(p5<0){
+                            alert("Adicione no minimo 1 produto")
+                            p5=0
+                        }
+                        quantidade5.innerHTML = p5
+                    }
+                    function adicionar2(){
+                        p2=p2+1
+                        quantidade2.innerHTML = p2
+                    
+                    }
+                    function diminuir2(){
+                            p2=p2-1
+                            if(p2<0){
+                                alert("Adicione no minimo 1 produto")
+                                p2=0
+                            }
+                            quantidade2.innerHTML = p2
+                        }
+                        function adicionar2(){
+                            p2=p2+1
+                            quantidade2.innerHTML = p2
+                        
+                        }
+                        function diminuir2(){
+                                p2=p2-1
+                                if(p2<0){
+                                    alert("Adicione no minimo 1 produto")
+                                    p2=0
+                                }
+                                quantidade2.innerHTML = p2
+                            }
+                            function adicionar2(){
+                                p2=p2+1
+                                quantidade2.innerHTML = p2
+                            
+                            }
+                            function diminuir2(){
+                                    p2=p2-1
+                                    if(p2<0){
+                                        alert("Adicione no minimo 1 produto")
+                                        p2=0
+                                    }
+                                    quantidade2.innerHTML = p2
+                                }
+                                function adicionar2(){
+                                    p2=p2+1
+                                    quantidade2.innerHTML = p2
+                                
+                                }
+                                function diminuir2(){
+                                        p2=p2-1
+                                        if(p2<0){
+                                            alert("Adicione no minimo 1 produto")
+                                            p2=0
+                                        }
+                                        quantidade2.innerHTML = p2
+                                    }
+                                    function adicionar2(){
+                                        p2=p2+1
+                                        quantidade2.innerHTML = p2
+                                    
+                                    }
+                                    function diminuir2(){
+                                            p2=p2-1
+                                            if(p2<0){
+                                                alert("Adicione no minimo 1 produto")
+                                                p2=0
+                                            }
+                                            quantidade2.innerHTML = p2
+                                        }
+                                        function adicionar2(){
+                                            p2=p2+1
+                                            quantidade2.innerHTML = p2
+                                        
+                                        }
+                                        function diminuir2(){
+                                                p2=p2-1
+                                                if(p2<0){
+                                                    alert("Adicione no minimo 1 produto")
+                                                    p2=0
+                                                }
+                                                quantidade2.innerHTML = p2
+                                            }
+                                            function adicionar2(){
+                                                p2=p2+1
+                                                quantidade2.innerHTML = p2
+                                            
+                                            }
+                                            function diminuir2(){
+                                                    p2=p2-1
+                                                    if(p2<0){
+                                                        alert("Adicione no minimo 1 produto")
+                                                        p2=0
+                                                    }
+                                                    quantidade2.innerHTML = p2
+                                            }
