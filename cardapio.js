28cb87734188b5bc3ff8c997381ae65ffc99b39f
Meliodasf143 <80070477+Meliodasf143@users.noreply.github.com>
2023-06-01 14:12:30 -0300

    Add files via upload

--- cardapio.js
+++ cardapio.js
@@ -1,29 +1,44 @@
-let p1=[0,15.99,"X-salada"]
-let p2=[0,"X-EGG",15.99]
-let p3=[0,"X-Bacon",19.99]
-let p4=[0,"X-Tudo",25.99]
-let p5=[0,"X-Picanha",29.99]
-let p6=[0,"Barata Frita",9.99]
-let p7=[0,"Suco Natural",7.99]
-let p8=[0,"Água 500ml",4.99]
-let p9=[0,"Coca Cola",7.99]
-let p10=[0,"Guarana",7.99]
-let p11=[0,"Cerveja",6.99]
+let p1=[0,"X-salada",15.99,0]
+let p2=[0,"X-EGG",15.99,0]
+let p3=[0,"X-Bacon",19.99,0]
+let p4=[0,"X-Tudo",25.99,0]
+let p5=[0,"X-Picanha",29.99,0]
+let p6=[0,"Barata Frita",9.99,0]
+let p7=[0,"Suco Natural",7.99,0]
+let p8=[0,"Água 500ml",4.99,0]
+let p9=[0,"Coca Cola",7.99,0]
+let p10=[0,"Guarana",7.99,0]
+let p11=[0,"Cerveja",6.99,0]
  t=0
  pe=1
-
+ function formatamoeda(numero){
+ return numero.toFixed(2).replace(".",",")}
 function adicionar(){
-    p1=p1+1
-    quantidade.innerHTML = p1
+    p1[0]=p1[0]+1
+  
+    t=t+1
+    total.innerHTML=t
+    quantidade1.innerHTML= p1[0]
+  
 
 }
 function diminuir(){
+    p1[0]=p1[0]-1
    
+            if(p1[0]<0){
+                alert("Adicione no minimo 1 produto")
+                p1[0]=0
    
     }
+    else{
+        t=t-1
+        total.innerHTML=t
+    }
+        quantidade1.innerHTML = p1[0]
+    }
     function adicionar2(){
-        alert('oi')
         p2[0]=p2[0]+1
+      
         quantidade2.innerHTML = p2[0]
         t=t+1
         total.innerHTML=t
@@ -131,7 +146,7 @@ function diminuir(){
                                     t=t-1
                                     total.innerHTML=t
                                 }
-                                quantidade7let .innerHTML = p7[0]
+                                quantidade7 .innerHTML = p7[0]
                             }
                             function adicionar8(){
                                 p8[0]=p8[0]+1
@@ -194,7 +209,7 @@ function diminuir(){
                                             total.innerHTML=t}
                                     
                                         function diminuir11(){
-                                                p11=p11[0]-1
+                                                p11[0]=p11[0]-1
                                                 if(p11[0]<0){
                                                     alert("Adicione no minimo 1 produto")
                                                     p11[0]=0
@@ -203,10 +218,19 @@ function diminuir(){
                                                     t=t-1
                                                     total.innerHTML=t
                                                 }
-                                                quantidade11.innerHTML = p11[0]}
+                                              
+                                                quantidade11.innerHTML = p11[0]
+                                            }
                                             
             function pedido(){
                 let el= document.getElementById('conta')
+                let es= document.getElementById('cardapiod')
+                let et= document.getElementById('pedir')
+                
+                es.style.cssText =
+                'background-color: #918a82'
+                et.style.cssText =
+                'background-color: #fff3e5;'
                 el.style.cssText =
                   'display: block;' 
                   if(p1[0]>0){
@@ -215,24 +239,27 @@ function diminuir(){
                     'display:block;'
                     quantidadep1.innerHTML= p1[0]+"x"
                     produto1.innerHTML= p1[1]
-                    preco1.innerHTML= "R$ "+p1[2]
-                    precototal1.innerHTML= "R$ "+ p1[2]*p1[0]}
+                    preco1.innerHTML= "R$ "+formatamoeda(p1[2])
+                    p1[3]=p1[0]*p1[2]
+                    precototal1.innerHTML= "R$ "+ formatamoeda(p1[3])}
                     if(p2[0]>0){
                         let es= document.getElementById('div2')
                         es.style.cssText =
                         'display:block;'
                         quantidadep2.innerHTML= p2[0]+"x"
                         produto2.innerHTML= p2[1]
-                        preco2.innerHTML= "R$ "+p2[2]
-                        precototal2.innerHTML= "R$ "+ p2[2]*p2[0]}
+                        preco2.innerHTML= "R$ "+formatamoeda(p2[2])
+                        p2[3]=p2[0]*p2[2]
+                        precototal2.innerHTML= "R$ "+ formatamoeda(p2[3])}
                   if(p3[0]>0){
                     let es= document.getElementById('div3')
                     es.style.cssText =
                     'display:block;'
                     quantidadep3.innerHTML= p3[0]+"x"
                     produto3.innerHTML= p3[1]
-                    preco3.innerHTML= "R$ "+p3[2]
-                    precototal3.innerHTML= "R$ "+ p3[2]*p3[0]
+                    preco3.innerHTML= "R$ "+formatamoeda(p3[2])
+                    p3[3]=p3[0]*p3[2]
+                    precototal3.innerHTML= "R$ "+ formatamoeda(p3[3])
                    
                      }
                      if(p4[0]>0){
@@ -241,74 +268,92 @@ function diminuir(){
                         'display:block;'
                         quantidadep4.innerHTML= p4[0]+"x"
                         produto4.innerHTML= p4[1]
-                        preco4.innerHTML= "R$ "+p4[2]
-                        precototal4.innerHTML= "R$ "+ p4[2]*p4[0]}
+                        preco4.innerHTML= "R$ "+formatamoeda(p4[2])
+                        p4[3]=p4[0]*p4[2]
+                        precototal4.innerHTML= "R$ "+ formatamoeda(p4[3])}
                      if(p5[0]>0){
                         let es= document.getElementById('div5')
                          es.style.cssText =
                           'display:block;'
                         quantidadep5.innerHTML= p5[0]+"x"
                          produto5.innerHTML= p5[1]
-                         preco5.innerHTML= "R$ "+p5[2]
-                       precototal5.innerHTML= "R$ "+ p5[2]*p5[0]}
+                         preco5.innerHTML= "R$ "+formatamoeda(p5[2])
+                         p5[3]=p5[0]*p5[2]
+                       precototal5.innerHTML= "R$ "+ formatamoeda(p5[3])}
                        if(p6[0]>0){
                         let es= document.getElementById('div6')
                          es.style.cssText =
                           'display:block;'
                         quantidadep6.innerHTML= p6[0]+"x"
                          produto6.innerHTML= p6[1]
-                         preco6.innerHTML= "R$ "+p6[2]
-                       precototal6.innerHTML= "R$ "+ p6[2]*p6[0]}
+                         preco6.innerHTML= "R$ "+formatamoeda(p6[2])
+                         p6[3]=p6[0]*p6[2]
+                       precototal6.innerHTML= "R$ "+ formatamoeda(p6[3])}
                        if(p7[0]>0){
                         let es= document.getElementById('div7')
                          es.style.cssText =
                           'display:block;'
                         quantidadep7.innerHTML= p7[0]+"x"
                          produto7.innerHTML= p7[1]
-                         preco7.innerHTML= "R$ "+p7[2]
-                       precototal7.innerHTML= "R$ "+ p7[2]*p7[0]}
+                         preco7.innerHTML= "R$ "+formatamoeda(p7[2])
+                         p7[3]=p7[0]*p7[2]
+                       precototal7.innerHTML= "R$ "+ formatamoeda(p8[2])}
                        if(p8[0]>0){
                         let es= document.getElementById('div8')
                          es.style.cssText =
                           'display:block;'
                         quantidadep8.innerHTML= p8[0]+"x"
                          produto8.innerHTML= p8[1]
-                         preco8.innerHTML= "R$ "+p8[2]
-                       precototal8.innerHTML= "R$ "+ p8[2]*p8[0]}
+                         preco8.innerHTML= "R$ "+formatamoeda(p8[2])
+                         p8[3]=p8[0]*p8[2]
+                       precototal8.innerHTML= "R$ "+ formatamoeda(p8[3])}
                        if(p9[0]>0){
                         let es= document.getElementById('div9')
                          es.style.cssText =
                           'display:block;'
                         quantidadep9.innerHTML= p9[0]+"x"
                          produto9.innerHTML= p9[1]
-                         preco9.innerHTML= "R$ "+p9[2]
-                       precototal9.innerHTML= "R$ "+ p9[2]*p9[0]}
+                         preco9.innerHTML= "R$ "+formatamoeda(p9[2])
+                         p9[3]=p9[0]*p9[2]
+                       precototal9.innerHTML= "R$ "+ formatamoeda(p9[3])}
                        if(p10[0]>0){
                         let es= document.getElementById('div10')
                          es.style.cssText =
                           'display:block;'
-                        quantidadep10.innerHTML= p5[0]+"x"
+                        quantidadep10.innerHTML= p10[0]+"x"
                          produto10.innerHTML= p10[1]
-                         preco10.innerHTML= "R$ "+p10[2]
-                       precototal10.innerHTML= "R$ "+ 105[2]*p10[0]}
+                         preco10.innerHTML= "R$ "+formatamoeda(p10[2])
+                         p10[3]=p10[0]*p10[2]
+                       precototal10.innerHTML= "R$ "+ formatamoeda(p10[3])}
                        if(p11[0]>0){
                         let es= document.getElementById('div11')
                          es.style.cssText =
                           'display:block;'
                         quantidadep11.innerHTML= p11[0]+"x"
                          produto11.innerHTML= p11[1]
-                         preco11.innerHTML= "R$ "+p11[2]
-                       precototal11.innerHTML= "R$ "+ p11[2]*p11[0]}
+                         preco11.innerHTML= "R$ "+formatamoeda(p11[2])
+                         p11[3]=p11[0]*p11[2]
+                       precototal11.innerHTML= "R$ "+ formatamoeda(p11[3])}
+                        vt=p1[3]+p2[3]+p3[3]+p4[3]+p5[3]+p6[3]+p7[3]+p8[3]+p9[3]+p10[3]+p11[3]
+                        tprod.innerHTML= t +'x produtos'
+                         vtotal.innerHTML='R$ '+formatamoeda(vt)
+                         vtc= vt-(vt*0.1)
+                         totavc.innerHTML= "Valor a a pagar RS"+formatamoeda(vtc)        
+
+}                 
 
 
-}                         
 
-               
             
-            function sair(){
+            function conta(){
              
                 let el= document.getElementById('conta')
+                let es= document.getElementById('cardapiod')
+                let et= document.getElementById('pedir')
+                es.style.cssText =
+                'background-color: #fff3e5;' 
+                 et.style.cssText =
+                 'background-color: #918a82'
                 el.style.cssText =
                   'display: none;' 
-                 
-             }
+            }
